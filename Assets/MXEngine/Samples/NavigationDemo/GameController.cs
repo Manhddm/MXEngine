0e63043d22b61ed195804e59f74e29ffec0bb3b0
Đồng Đức Mạnh <148237861+Manhddm@users.noreply.github.com>
2026-09-25 07:39:18 +0700

    refactor: remove view catalog from game controller

--- Assets/MXEngine/Samples/NavigationDemo/GameController.cs
+++ Assets/MXEngine/Samples/NavigationDemo/GameController.cs
@@ -6,17 +6,26 @@ namespace MXEngine.Samples
     public class GameController : MonoBehaviour
     {
         public static GameController Instance { get; private set; }
+
         [SerializeField] private Canvas canvas;
-        [SerializeField] private ViewCatalog viewCatalog;
         [SerializeField] private UIRoot uiRoot;
+
         private NavigationService _navigation;
+
         private Canvas Canvas => canvas;
         public NavigationService Navigation => _navigation;
+
         private void Awake()
         {
-            if (Instance == null) Instance = this;
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
             DontDestroyOnLoad(gameObject);
-            _navigation = new NavigationService(new AddressableViewLoader(), viewCatalog, uiRoot);
+            _navigation = new NavigationService(new AddressableViewLoader(), uiRoot);
         }
     }
 }
