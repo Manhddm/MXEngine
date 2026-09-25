f2f123c4b08f4d5fa834183f1606024e4705440a
Đồng Đức Mạnh <148237861+Manhddm@users.noreply.github.com>
2026-09-25 07:39:11 +0700

    test: remove catalog contract coverage

--- Assets/MXEngine/Tests/CoreContractTests.cs
+++ Assets/MXEngine/Tests/CoreContractTests.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Reflection;
-using MXEngine.MVP;
 using NUnit.Framework;
-using UnityEngine;
 
 namespace MXEngine.Tests
 {
@@ -15,23 +11,6 @@ namespace MXEngine.Tests
             var state = new TestState { FailDispose = true };
             Assert.Throws<InvalidOperationException>(() => state.Dispose());
             Assert.DoesNotThrow(() => state.Dispose());
-        }
-
-        [Test]
-        public void DuplicateCatalogIdIsRejected()
-        {
-            var catalog = ScriptableObject.CreateInstance<ViewCatalog>();
-            try
-            {
-                typeof(ViewCatalog).GetField("entries", BindingFlags.Instance | BindingFlags.NonPublic)
-                    .SetValue(catalog, new List<ViewEntry>
-                    {
-                        new ViewEntry { Id = 7, Layer = ViewLayer.Screen },
-                        new ViewEntry { Id = 7, Layer = ViewLayer.Modal }
-                    });
-                Assert.Throws<InvalidOperationException>(() => catalog.Get(7));
-            }
-            finally { UnityEngine.Object.DestroyImmediate(catalog); }
         }
     }
 }
