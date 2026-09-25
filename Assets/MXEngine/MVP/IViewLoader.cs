0b1872363f9111fb340807c0d9424341a9f54e0b
Đồng Đức Mạnh <148237861+Manhddm@users.noreply.github.com>
2026-09-25 07:36:37 +0700

    refactor: load views by address

--- Assets/MXEngine/MVP/IViewLoader.cs
+++ Assets/MXEngine/MVP/IViewLoader.cs
@@ -1,15 +1,15 @@
 using Cysharp.Threading.Tasks;
 using System.Threading;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
 
 namespace MXEngine.MVP
 {
     public interface IViewLoader
     {
         UniTask<T> LoadAsync<T>(
-            AssetReferenceGameObject reference,
-            Transform parent, CancellationToken cancellationToken = default)
+            string address,
+            Transform parent,
+            CancellationToken cancellationToken = default)
             where T : Component;
 
         UniTask ReleaseAsync(Component view);
